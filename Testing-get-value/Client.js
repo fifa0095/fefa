5440c1eb4eca4774ff448eff5ff92176b07f5b24
unknown <patnj18@gmail.com>
2021-02-25 15:41:27 +0700

    Adding Mini game page

---
+++ Testing-get-value/Client.js
@@ -0,0 +1,25 @@
+
+const button1 = document.getElementById("Char1")
+const button2 = document.getElementById("Char2")
+const button3 = document.getElementById("Char3")
+const button4 = document.getElementById("Char4")
+
+const button5 = document.getElementById("Summit")
+
+let cretedCha = [];
+
+button1.onclick = function(){
+    cretedCha.push("A");
+}
+
+button2.onclick = function(){
+    cretedCha.push("B");
+}
+
+button3.onclick = function(){
+    cretedCha.push("C");
+}
+
+button4.onclick = function(){
+    cretedCha.push("D");
+}
