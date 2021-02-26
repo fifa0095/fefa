00bfd7dc377e9de48ec1a6d3f05f50382eebba7e
unknown <patnj18@gmail.com>
2021-02-26 23:07:27 +0700

    update HTML file and adding List counter

--- Testing-get-value/Client.js
+++ Testing-get-value/Client.js
@@ -1,4 +1,5 @@
 
+// call element on html pages
 const button1 = document.getElementById("Char1")
 const button2 = document.getElementById("Char2")
 const button3 = document.getElementById("Char3")
@@ -6,20 +7,62 @@ const button4 = document.getElementById("Char4")
 
 const button5 = document.getElementById("Summit")
 
-let cretedCha = [];
+const button6 = document.getElementById('Reject')
 
-button1.onclick = function(){
+const List = document.getElementById("Element")
+
+
+// create arrays for storing input
+let cretedCha = [];
+let sentChar = [];
+
+//add event listener when the button was cliked
+button1.onpointerdown = function(){
     cretedCha.push("A");
 }
 
-button2.onclick = function(){
+button2.onpointerdown = function(){
     cretedCha.push("B");
 }
 
-button3.onclick = function(){
+button3.onpointerdown = function(){
     cretedCha.push("C");
 }
 
-button4.onclick = function(){
+button4.onpointerdown = function(){
     cretedCha.push("D");
 }
+
+//add remove button
+button6.onpointerdown = function(){
+    sentChar.pop();
+}
+
+
+// remove array value function
+function removeItem(arr, item) {
+    return arr.filter(f => f !== item)
+}
+
+// create function for check input
+function CheckArray(){
+    //create statement for getting 4 random characters in 1 array
+    //update Input list
+    if (cretedCha.length === 4){
+        //put to main Array
+        sentChar.push(cretedCha);
+        
+        
+        //reset array
+        cretedCha = [];
+    }
+    
+    //check for Input list has 5 array
+    if (sentChar.length === 5){
+        clearInterval(checkIt);
+    }
+    //Input List
+    List.textContent = sentChar.join(' | ')
+}
+
+const checkIt= setInterval(CheckArray, 1000);
